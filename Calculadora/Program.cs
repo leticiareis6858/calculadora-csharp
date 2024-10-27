dbe791592b18fb7fdd1aa97d92b1c867bab893de
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-10-27 20:21:45 -0300

    feat: faz uso de switch case e cria menu interativo melhorado.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,46 +1,121 @@
-﻿Console.WriteLine("Aviso: números decimais devem ser informados com virgula!");
-
-Console.WriteLine("Informe um número:");
-double primeiroNumero = 0;
-primeiroNumero = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe outro número:");
-double segundoNumero = 0;
-segundoNumero = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a operação desejada:\n" +
-    "1. Adição\n" +
-    "2. Subtração\n" +
-    "3. Multiplicação\n" +
-    "4. Divisão");
-
-int operacao = int.Parse(Console.ReadLine());
-
-if(operacao == 1)
+﻿class Calculadora
 {
-    double adicao = primeiroNumero + segundoNumero;
-    adicao = Math.Round(adicao, 2);
-    Console.WriteLine("O resultado da adição de: " + primeiroNumero + " + " + segundoNumero + " é igual a: " + adicao);
-}
-else if (operacao == 2)
-{
-    double subtracao = primeiroNumero - segundoNumero;
-    subtracao = Math.Round(subtracao, 2);
-    Console.WriteLine("O resultado da subtração de: " + primeiroNumero + " - " + segundoNumero + " é igual a: " + subtracao);
-}
-else if (operacao == 3)
-{
-    double multiplicacao = primeiroNumero * segundoNumero;
-    multiplicacao = Math.Round(multiplicacao, 2);
-    Console.WriteLine("O resultado da multiplicação de: " + primeiroNumero + " x " + segundoNumero + " é igual a: " + multiplicacao);
-}
-else if (operacao == 4)
-{
-    double divisao = primeiroNumero / segundoNumero;
-    divisao = Math.Round(divisao, 2);
-    Console.WriteLine("O resultado da divisão de: " + primeiroNumero + " por " + segundoNumero + " é igual a: " + divisao);
-}
-else
-{
-    Console.WriteLine("Opção inválida. Informe apenas o número referente a operação desejada!");
+    static double Adicao(double primeiroNumero, double segundoNumero)
+    {
+        return Math.Round(primeiroNumero + segundoNumero, 2);
+    }
+
+    static double Subtracao(double primeiroNumero, double segundoNumero)
+    {
+        return Math.Round(primeiroNumero - segundoNumero, 2);
+    }
+
+    static double Divisao(double primeiroNumero, double segundoNumero)
+    {
+        if (segundoNumero == 0)
+        {
+            throw new DivideByZeroException("Não é possível dividir por zero.");
+        }
+        return Math.Round(primeiroNumero / segundoNumero, 2);
+    }
+
+    static double Multiplicacao(double primeiroNumero, double segundoNumero)
+    {
+        return Math.Round(primeiroNumero * segundoNumero, 2);
+    }
+
+    static void Main()
+    {
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.WriteLine("Aviso: números decimais devem ser informados com virgula!");
+
+            Console.WriteLine("Informe um número:");
+            double primeiroNumero = 0;
+            primeiroNumero = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe outro número:");
+            double segundoNumero = 0;
+            segundoNumero = double.Parse(Console.ReadLine());
+
+            bool opcao = true;
+
+            while (opcao)
+            {
+                Console.WriteLine("Informe a operação desejada:\n" +
+                "1. Adição\n" +
+                "2. Subtração\n" +
+                "3. Multiplicação\n" +
+                "4. Divisão\n"+
+                "0. Sair");
+
+                int operacao = int.Parse(Console.ReadLine());
+
+                if (operacao == 0)
+                {
+                    opcao = false;
+                    continuar = false;
+                    Console.WriteLine("Encerrando...");
+                    break;
+                }
+
+                double resultado;
+
+                switch (operacao)
+                {
+                    case 1:
+                        resultado = Adicao(primeiroNumero, segundoNumero);
+                        Console.WriteLine("O resultado da adição de: " + primeiroNumero + " + " + segundoNumero + " é igual a: " + resultado);
+                        break;
+                    case 2:
+                        resultado = Subtracao(primeiroNumero, segundoNumero);
+                        Console.WriteLine("O resultado da subtração de: " + primeiroNumero + " - " + segundoNumero + " é igual a: " + resultado);
+                        break;
+                    case 3:
+                        resultado = Multiplicacao(primeiroNumero, segundoNumero);
+                        Console.WriteLine("O resultado da multiplicação de: " + primeiroNumero + " x " + segundoNumero + " é igual a: " + resultado);
+                        break;
+                    case 4:
+                        try
+                        {
+                            resultado = Divisao(primeiroNumero, segundoNumero);
+                            Console.WriteLine("O resultado da divisão de: " + primeiroNumero + " por " + segundoNumero + " é igual a: " + resultado);
+                        }
+                        catch (DivideByZeroException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        continue;
+                }
+
+                Console.WriteLine("O que deseja fazer agora?\n" +
+                  "1. Fazer outra operação com os mesmos números\n" +
+                  "2. Informar novos números\n" +
+                  "3. Encerrar");
+                int escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        continue;
+                    case 2:
+                        opcao = false;
+                        break;
+                    case 3:
+                        opcao = false;
+                        continuar = false;
+                        Console.WriteLine("Encerrando...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        continue;
+                }
+            }
+        }
+    }
 }
--- Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e8d97d2a2cfdfa69e9ad4d4884ff53ae6a77a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
